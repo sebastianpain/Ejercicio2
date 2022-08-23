1a7ee31a2888817ba672a465549e10b902b98e55
Sebastian Pain <110700982+sebastianpain@users.noreply.github.com>
2022-08-23 22:17:10 +0000

    Add files via upload

---
+++ Ejercicio2.js
@@ -0,0 +1,26 @@
+const lista = ["Sebastian", 33, true, 'Junary 12 1989', 'libro'];
+
+const minombre = "Sebastian";
+console.log(minombre)
+
+const miedad = 33;
+console.log(miedad)
+
+
+const desarrollador = true;
+console.log(desarrollador);
+
+const date = 'Junary 12 1989'
+console.log(date);
+const libro = {
+    Titulo : "Otra vuelta de tuerca",
+    autor:"Henry James",
+    fecha:1898,
+    url: "https://articulo.mercadolibre.com.ar/MLA-695845746-otra-vuelta-de-tuerca-3ra-edicion-_JM?matt_tool=46319439&matt_word=&matt_source=google&matt_campaign_id=14600917913&matt_ad_group_id=127394558536&matt_match_type=&matt_network=g&matt_device=c&matt_creative=544876793743&matt_keyword=&matt_ad_position=&matt_ad_type=pla&matt_merchant_id=553579586&matt_product_id=MLA695845746&matt_product_partition_id=1433669517002&matt_target_id=pla-1433669517002&gclid=Cj0KCQjw9ZGYBhCEARIsAEUXITWTXz5okALeoJ-zsOewDRHURnQmJGxAhxmixiAg0O-kk9ZBfPbSXOIaAqxkEALw_wcB",
+    
+
+} 
+console.log(libro.Titulo)
+console.log(libro.autor)
+console.log(libro.fecha)
+console.log(libro.url)
